302b7ae628620c3092b6c2cc9343cfb1b542067c
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-03-07 21:35:16 -0500

    Update CreateAccount.js
    - HTML update

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,73 +1,72 @@
 import React from 'react'
 
-const CreateAccountPage
- = () => {
+const CreateComponent = () => {
     return (
         <div>
-            <div class="account-section-dark">
-        <div class="bg-animation">
-          <div class="preloader"><img src="images/Union-1.svg" width="50" height="Auto" alt="" class="preloader-logo"/></div>
-          <div class="section-foundation-header header-gradient-01 wf-section">
+            <div className="account-section-dark">
+        <div className="bg-animation">
+          <div className="preloader"><img src="images/Union-1.svg" width="50" height="Auto" alt="" className="preloader-logo"/></div>
+          <div className="section-foundation-header header-gradient-01 wf-section">
             <div>
-              <div class="changelog-header-padding">
-                <div class="flighing-shape-wrapper">
-                  <div data-w-id="d853071b-b591-b511-84ab-8026b424025d" style={{opacity:0}} class="flighing-shape _07"></div>
-                  <div data-w-id="d853071b-b591-b511-84ab-8026b424025e" style={{opacity:0}} class="flighing-shape _06"></div>
-                  <div data-w-id="d853071b-b591-b511-84ab-8026b424025f" style={{opacity:0}} class="flighing-shape _05"></div>
-                  <div class="flighing-shape _04"></div>
-                  <div data-w-id="d853071b-b591-b511-84ab-8026b4240261" style={{opacity:0}} class="flighing-shape _03"></div>
-                  <div data-w-id="d853071b-b591-b511-84ab-8026b4240262" style={{opacity:0}} class="flighing-shape _02"></div>
-                  <div data-w-id="d853071b-b591-b511-84ab-8026b4240263" style={{opacity:0}} class="flighing-shape"></div>
+              <div className="changelog-header-padding">
+                <div className="flighing-shape-wrapper">
+                  <div data-w-id="d853071b-b591-b511-84ab-8026b424025d"  className="flighing-shape _07"/>
+                  <div data-w-id="d853071b-b591-b511-84ab-8026b424025e" className="flighing-shape _06"/>
+                  <div data-w-id="d853071b-b591-b511-84ab-8026b424025f"  className="flighing-shape _05"/>
+                  <div className="flighing-shape _04"></div>
+                  <div data-w-id="d853071b-b591-b511-84ab-8026b4240261"className="flighing-shape _03"/>
+                  <div data-w-id="d853071b-b591-b511-84ab-8026b4240262"  className="flighing-shape _02"/>
+                  <div data-w-id="d853071b-b591-b511-84ab-8026b4240263"className="flighing-shape"/>
                 </div>
               </div>
             </div>
-            <div class="blurs">
-              <div data-w-id="d853071b-b591-b511-84ab-8026b4240265" ></div>
-              <div data-w-id="d853071b-b591-b511-84ab-8026b4240266" ></div>
-              <div class="gradient-yellow-2"></div>
-              <div class="gradient-red-2"></div>
-              <div class="gradient-red-2"></div>
-              <div class="gradient-red-2"></div>
+            <div className="blurs">
+            <div data-w-id="d853071b-b591-b511-84ab-8026b4240265" className="gradient-orange"/>
+            <div data-w-id="d853071b-b591-b511-84ab-8026b4240266" className="gradient-red"/>
+           <div className="gradient-yellow-2"/>
+              <div className="gradient-red-2"/>
+              <div className="gradient-red-2"/>
+              <div className="gradient-red-2"/>
             </div>
           </div>
         </div>
-        <div class="account-wrapper-dark">
-          <div class="account-card-dark">
+        <div className="account-wrapper-dark">
+          <div className="account-card-dark">
             <div>
-              <div class="account-header-wrapper">
-                <h2 class="account-heading">Get Started</h2>
+              <div className="account-header-wrapper">
+                <h2 className="account-heading">Get Started</h2>
                 <p>We are excited to have you!</p>
-                <p class="paragraph-regular text-weight-medium">Already have an account? <a href="index.html" class="account-link">Login</a>
+                <p className="paragraph-regular text-weight-medium">Already have an account? <a href="index.html" className="account-link">Login</a>
                 </p>
               </div>
-              <div class="w-form">
+              <div className="w-form">
                 <form id="email-form" name="email-form" data-name="Email Form" method="get">
-                  <div class="w-layout-grid grid-one-column">
-                    <div class="account-wrapper">
-                      <div class="account-field-label">Enter your Name, Company, Email and Password</div>
-                      <div class="account-icon-wrapper"><input type="text" class="account-text-field w-input" maxlength="256" name="Account-Name" data-name="Account Name" placeholder="Enter your Name" id="Account-Name"/></div>
-                      <div class="account-icon-wrapper"><input type="text" class="account-text-field w-input" maxlength="256" name="Account-Company" data-name="Account Company" placeholder="Enter your company" id="Account-Company-3"/></div>
-                      <div class="account-icon-wrapper"><input type="email" class="account-text-field w-input" maxlength="256" name="Account-Email-2" data-name="Account Email 2" placeholder="Enter your email" id="Account-Email-2"/></div>
-                      <div class="account-icon-wrapper"><input type="password" class="account-text-field w-input" maxlength="256" name="Account-Password" data-name="Account Password" placeholder="Enter your password" id="Account-Password" required=""/>
-                      <input type="submit" value="" data-wait="Please wait..." class="account-submit w-button" />
-                        <div class="account-arrow w-embed"><svg width="40" height="40" viewbox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <div className="w-layout-grid grid-one-column">
+                    <div className="account-wrapper">
+                      <div className="account-field-label">Enter your Name, Company, Email and Password</div>
+                      <div className="account-icon-wrapper"><input type="text" className="account-text-field w-input" maxLength="256" name="Account-Name" data-name="Account Name" placeholder="Enter your Name" id="Account-Name"/></div>
+                      <div className="account-icon-wrapper"><input type="text" className="account-text-field w-input" maxLength="256" name="Account-Company" data-name="Account Company" placeholder="Enter your company" id="Account-Company-3"/></div>
+                      <div className="account-icon-wrapper"><input type="email" className="account-text-field w-input" maxLength="256" name="Account-Email-2" data-name="Account Email 2" placeholder="Enter your email" id="Account-Email-2"/></div>
+                      <div className="account-icon-wrapper"><input type="password" className="account-text-field w-input" maxLength="256" name="Account-Password" data-name="Account Password" placeholder="Enter your password" id="Account-Password" required=""/>
+                      <input type="submit" value="" data-wait="Please wait..." className="account-submit w-button" />
+                        <div className="account-arrow w-embed"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect width="40" height="40" rx="20" fill="#211f2c"></rect>
-                            <path d="M18.0762 24.6281L22.7242 20.0001L18.0762 15.3721" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            <path d="M18.0762 24.6281L22.7242 20.0001L18.0762 15.3721" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                           </svg></div>
                       </div>
                     </div>
                   </div>
-                  <div class="account-seperator">
-                    <div class="account-line"></div>
-                    <div class="text-block">OR</div>
-                    <div class="account-line"></div>
+                  <div className="account-seperator">
+                    <div className="account-line"></div>
+                    <div className="text-block">OR</div>
+                    <div className="account-line"></div>
                   </div>
-                  <div class="w-layout-grid account-social-grid">
-                    <div class="account-icon-wrapper">
-                      <a id="w-node-_6f815b0a-4b68-50a5-cd24-17d76333bbc8-6c243ac4" href="#" class="account-social-button w-inline-block">
+                  <div className="w-layout-grid account-social-grid">
+                    <div className="account-icon-wrapper">
+                      <a id="w-node-_6f815b0a-4b68-50a5-cd24-17d76333bbc8-6c243ac4" href="#" className="account-social-button w-inline-block">
                         <div>Continue with Google</div>
                       </a>
-                      <div class="account-logo w-embed"><svg width="32" height="32" viewbox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <div className="account-logo w-embed"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M30.0014 16.3109C30.0014 15.1598 29.9061 14.3198 29.6998 13.4487H16.2871V18.6442H24.1601C24.0014 19.9354 23.1442 21.8798 21.2394 23.1864L21.2127 23.3604L25.4536 26.58L25.7474 26.6087C28.4458 24.1665 30.0014 20.5731 30.0014 16.3109Z" fill="#4285F4"></path>
                           <path d="M16.2853 30C20.1424 30 23.3804 28.7555 25.7456 26.6089L21.2377 23.1865C20.0313 24.011 18.4123 24.5866 16.2853 24.5866C12.5076 24.5866 9.30128 22.1444 8.15832 18.7688L7.99078 18.7827L3.58111 22.1272L3.52344 22.2843C5.87261 26.8577 10.698 30 16.2853 30Z" fill="#34A853"></path>
                           <path d="M8.16061 18.7688C7.85903 17.8977 7.6845 16.9643 7.6845 15.9999C7.6845 15.0354 7.85903 14.1021 8.14475 13.231L8.13676 13.0455L3.67181 9.64734L3.52572 9.71544C2.55751 11.6132 2.00195 13.7444 2.00195 15.9999C2.00195 18.2555 2.55751 20.3865 3.52572 22.2843L8.16061 18.7688Z" fill="#FBBC05"></path>
@@ -76,25 +75,25 @@ const CreateAccountPage
                     </div>
                   </div>
                 </form>
-                <div class="contact-success-message w-form-done">
+                <div className="contact-success-message w-form-done">
                   <div>Thank you! Your submission has been received!</div>
                 </div>
-                <div class="w-form-fail">
+                <div className="w-form-fail">
                   <div>Oops! Something went wrong while submitting the form.</div>
                 </div>
               </div>
-              <div class="account-caption">This site is protected by reCAPTCHA and the Hexigon&#x27;s <a href="#" class="account-link-small">Privacy Policy</a> and Terms of Service apply as an Hexigon AI, Inc. Product.</div>
+              <div className="account-caption">This site is protected by reCAPTCHA and the Hexigon&#x27;s <a href="#" className="account-link-small">Privacy Policy</a> and Terms of Service apply as an Hexigon AI, Inc. Product.</div>
             </div>
           </div>
-          <div class="div-block-9"><img src="images/hexigon-ai-logo.svg" loading="lazy" width="100" alt="" class="hexigon-ai-logo"/>
-            <div class="account-link-row">
-              <a href="http://hexigon.ai" class="account-link-side">Go to Hexigon.ai</a>
-              <div class="account-dot"></div>
-              <a href="http://hexigon.ai/support" class="account-link-side">Compo Support</a>
-              <div class="account-dot"></div>
-              <a href="http://hexigon.ai/terms" class="account-link-side">Terms</a>
-              <div class="account-dot"></div>
-              <a href="http://hexigon.ai/privacy" class="account-link-side">Privacy</a>
+          <div className="div-block-9"><img src="images/hexigon-ai-logo.svg" loading="lazy" width="100" alt="" className="hexigon-ai-logo"/>
+            <div className="account-link-row">
+              <a href="http://hexigon.ai" className="account-link-side">Go to Hexigon.ai</a>
+              <div className="account-dot"></div>
+              <a href="http://hexigon.ai/support" className="account-link-side">Compo Support</a>
+              <div className="account-dot"></div>
+              <a href="http://hexigon.ai/terms" className="account-link-side">Terms</a>
+              <div className="account-dot"></div>
+              <a href="http://hexigon.ai/privacy" className="account-link-side">Privacy</a>
             </div>
           </div>
         </div>
@@ -103,4 +102,4 @@ const CreateAccountPage
       )
     }
 
-export default CreateAccountPage
+export default CreateComponent
