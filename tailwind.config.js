341e50206f1df92535f9c9a0ed064b8f51b6b96a
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-03-09 01:03:00 -0500

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+      ],
+    theme: {
+      extend: {
+        colors: {
+        },
+        fontSize: {
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
