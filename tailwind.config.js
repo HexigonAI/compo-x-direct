7e4937266356f608bcfd684728b611310d7fe0a3
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-03-12 15:59:07 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-      ],
-    theme: {
-      extend: {
-        colors: {
-        },
-        fontSize: {
-        },
-      },
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
