26c02be30a5f722acd09d5625221a177f3c87420
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2023-03-12 16:02:10 -0400

    Query logic for post/fetch
    - Logic added to fetch user data and add users

---
+++ queries/queries.js
@@ -0,0 +1,22 @@
+import fetchData from "../helpers/fetchData";
+
+export const getHomepagePosts = async () => {
+    const data = await fetchData(
+        `
+        query HomepagePosts {
+            posts {
+                id
+                title
+                slug
+                featured_image
+                body
+            }
+        }
+        `,
+        {
+            variables: {}
+        }
+    )
+
+    return data.data.posts
+}
