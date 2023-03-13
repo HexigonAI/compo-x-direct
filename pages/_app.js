eb26ec3733dc67fc4c7c64978c9d5b237a4e6d77
nbrandon62 <nbrandon62@gmail.com>
2023-03-13 16:38:45 -0700

    scaffolding login authentication. login-page not working.

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,22 @@ import '../styles/compox-trim.webflow.css';
 import '../styles/webflow.css';
 import '../styles/normalize.css';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { SessionProvider } from 'next-auth/react'
+
 import Header from '@/components/Header';
 
 const queryClient = new QueryClient()
 
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps: { session, ...pageProps} }) {
   return (
     <>   
       <QueryClientProvider client={queryClient}>
+        <SessionProvider session={session}>
         {/* <Header/> */}
         <Component {...pageProps} />
+        </SessionProvider>
+
       </QueryClientProvider>
     </>  
   )
---
+++ pages/user-area.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserArea = () => {
+  return (
+    <div>This is an authenticated page</div>
+  )
+}
+
+export default UserArea
