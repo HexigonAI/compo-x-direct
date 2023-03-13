bcbf80cb10cf05a6546a47d6fc8ca5e98f842e78
nbrandon62 <nbrandon62@gmail.com>
2023-03-13 15:29:58 -0700

    cleaned up unused styling and imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// import styles from '@/styles/Home.module.css'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import {useQuery} from 'react-query';
 import { getHomepagePosts } from '@/queries/queries';
 
@@ -9,12 +7,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const {status, data: posts, error, isFetching, isSuccess} = useQuery("posts", async () =>  await getHomepagePosts())
+
   console.log(posts)
 
   return (
-    < div classname=" bg-black">
-          {isSuccess && posts.map(post => <div> {post.title} </div> )}
-
+    <div>
+          {isSuccess && posts.map(post => <div key={post.id}> {post.title} </div> )}
     </div>
   )
 }
