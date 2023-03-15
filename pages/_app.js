38961f2d22c9733d0132e6837b1598b75f3bd60d
nbrandon62 <nbrandon62@gmail.com>
2023-03-15 16:13:05 -0700

    renamed create-account to be index.js since it's our homepage

--- pages/_app.js
+++ pages/_app.js
@@ -3,24 +3,22 @@ import '../styles/compox-trim.webflow.css';
 import '../styles/webflow.css';
 import '../styles/normalize.css';
 import { QueryClientProvider, QueryClient } from 'react-query';
-import { SessionProvider } from 'next-auth/react'
-
-import Header from '@/components/Header';
-
-const queryClient = new QueryClient()
+import { SessionProvider } from 'next-auth/react';
 
 
-export default function App({ Component, pageProps: { session, ...pageProps} }) {
+const queryClient = new QueryClient();
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <>   
+    <>
       <QueryClientProvider client={queryClient}>
         <SessionProvider session={session}>
-        {/* <Header/> */}
-        <Component {...pageProps} />
+          <Component {...pageProps} />
         </SessionProvider>
-
       </QueryClientProvider>
-    </>  
-  )
-  
+    </>
+  );
 }
