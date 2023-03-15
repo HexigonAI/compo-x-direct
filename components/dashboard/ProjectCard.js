3ef1c532e0c124b8bf4b75c95d0c06b0f16a5ee8
nbrandon62 <nbrandon62@gmail.com>
2023-03-15 16:11:52 -0700

    added a reusable card that takes collection data as props

---
+++ components/dashboard/ProjectCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ProjectCard = ({projectTitle, icon, route}) => {
+  return (
+    <div>
+    {/* TODO add a route prop for respective projectID */}
+      <a
+        href='/site-dashboard/organization-grid'
+        class='account-item w-inline-block'
+      >
+        <div class='account'>
+          <div class='avatar-2 bg-1'>
+            <img src={icon} alt='' class='org-avatar' />
+          </div>
+          <div class='account-user-datails'>
+            <h6 class='mb-0'>
+              <strong>{projectTitle}</strong>
+            </h6>
+            <div class='hint'>Free Server</div>
+          </div>
+          <div class='label-item'>Active</div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default ProjectCard;
