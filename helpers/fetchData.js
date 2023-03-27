05181dd26cc3da7a558a5fca4361532cf2058ead
nbrandon62 <nbrandon62@gmail.com>
2023-03-27 13:21:32 -0700

    created a fetchUser() to abstract user fecth from fetchData()

--- helpers/fetchData.js
+++ helpers/fetchData.js
@@ -1,9 +1,34 @@
 const graphQLAPI = process.env.NEXT_PUBLIC_GRAPHQL;
+
 import { useSession } from 'next-auth/react';
 import { getCurrentUser } from '@/queries/Users';
 import { useQuery } from 'react-query';
 
-const fetchData = async (query, token, { variables = {} }) => {
+const fetchData = async (query, { variables = {} }, token ) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+
+  const res = await fetch(graphQLAPI, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  const json = await res.json();
+
+  if (json.errors) {
+    throw new Error(json.errors);
+  }
+
+  return json;
+};
+
+const fetchUser = async (query, token, { variables = {} }) => {
   const headers = {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${token}`,
@@ -27,29 +52,16 @@ const fetchData = async (query, token, { variables = {} }) => {
   return json.data.users_me;
 };
 
-export function fetchUser() {
-  const { data: session, status } = useSession({
-    required: true,
-  });
+export function getUser(token) {
+  // const { data: session, status } = useSession({
+  //   required: true,
+  // });
 
-  const { data: user, isSuccess } = useQuery('currentUser', async () => await fetchData(getCurrentUser, session.user.accessToken, {}), {
-    enabled: status === 'authenticated',
-  });
-
-  console.log(user)
-  console.log(session);
-  console.log(isSuccess);
+  const { data: user, isSuccess } = useQuery('currentUser', async () => await fetchUser(getCurrentUser, token, {}) );
+  
+  // console.log(isSuccess);
 
   return user
 }
-
-
-
-
-
-export function UserAvatar() {
-
-}
-
 
 export default fetchData;
