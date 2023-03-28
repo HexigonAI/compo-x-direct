0f3c72894b3dd748b318764a0f18a1046cc3c055
nbrandon62 <nbrandon62@gmail.com>
2023-03-28 13:11:13 -0700

    abstracted the fetchUser helper into it's own file for clarity

---
+++ helpers/fetchUser.js
@@ -0,0 +1,25 @@
+const graphQLAPI = process.env.NEXT_PUBLIC_GRAPHQL;
+
+export const fetchUser = async (query, token, { variables = {} }) => {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    };
+  
+    const res = await fetch(graphQLAPI + '/system', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    });
+  
+    const json = await res.json();
+  
+    if (json.errors) {
+      throw new Error(json.errors);
+    }
+  
+    return json.data.users_me;
+  };
