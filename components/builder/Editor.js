046960aebd76df094db8aba704746f2d68a430b1
nbrandon62 <nbrandon62@gmail.com>
2023-04-14 11:33:31 -0700

    abstracted the Editor to it's own component

---
+++ components/builder/Editor.js
@@ -0,0 +1,160 @@
+import { useEffect } from 'react';
+import grapesjs from 'grapesjs';
+import gsWebpage from 'grapesjs-preset-webpage';
+import gsNewsLetter from 'grapesjs-preset-newsletter';
+import gsCustome from 'grapesjs-custom-code';
+import axios from 'axios';
+import 'grapesjs/dist/css/grapes.min.css';
+
+import { addButton, icon, pagesSelect, publishSelect } from './Panels';
+
+const Editor = ({ token, id, projectEndpoint, handleSetEditor }) => {
+  useEffect(() => {
+    const editor = grapesjs.init({
+      container: '#gjs',
+      height: '100vh',
+      width: 'auto',
+      plugins: [gsWebpage, gsCustome, gsNewsLetter],
+      storageManager: {
+        id: 'gjs-',
+        type: 'remote',
+        options: {
+          remote: {
+            urlLoad: projectEndpoint,
+            urlStore: projectEndpoint,
+            fetchOptions: (opts) =>
+              opts.method === 'POST' ? { method: 'PATCH' } : {},
+            onStore: '',
+            onLoad: (result) => result,
+          },
+        },
+        autoload: true,
+        stepsBeforeSave: 3,
+        contentTypeJson: true,
+        storeComponents: true,
+        storeStyles: true,
+        storeHtml: true,
+        storeCss: true,
+      },
+      panels: { defaults: null },
+      deviceManager: {
+        devices: [
+          {
+            id: 'desktop',
+            name: 'Desktop',
+            width: '',
+          },
+          {
+            id: 'tablet',
+            name: 'Tablet',
+            width: '768px',
+            widthMedia: '992px',
+          },
+          {
+            id: 'mobilePortrait',
+            name: 'Mobile portrait',
+            width: '320px',
+            widthMedia: '575px',
+          },
+        ],
+      },
+      pluginsOpts: {
+        gsWebpage: {
+          blocksBasicOpts: {
+            blocks: [
+              'column1',
+              'column2',
+              'column3',
+              'column3-7',
+              'text',
+              'link',
+              'image',
+              'video',
+            ],
+            flexGrid: 1,
+          },
+          blocks: ['link-block', 'quote', 'text-basic'],
+        },
+      },
+    });
+
+    const projectData = editor.getProjectData();
+
+    editor.loadProjectData(projectData);
+    handleSetEditor(editor);
+
+    editor.Storage.add('remote', {
+        async load(options = {}) {
+          const fetchData = await axios.get(
+            `https://compo.directus.app/items/projects/${id}`
+          );
+          const builder_data = fetchData.data.data.builder_data;
+          const builder_string = builder_data.substring(
+            1,
+            builder_data.length - 1
+          );
+          return JSON.parse(builder_string);
+        },
+        async store(data) {
+          const sentData = JSON.stringify(data);
+          try {
+            axios.patch(
+              `https://compo.directus.app/items/projects/${id}`,
+              {
+                builder_data: `"${sentData}"`,
+              },
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+          } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+          }
+        },
+      });
+
+    editor.Panels.addPanel(icon);
+    editor.Panels.addPanel(pagesSelect);
+    editor.Panels.addPanel(publishSelect);
+    editor.Panels.addButton('options', addButton);
+
+    let arrButton = editor.Panels.getPanel('options').attributes.buttons.models;
+    let elementPrompt = arrButton[arrButton.length - 1];
+    arrButton.splice(arrButton.length - 1, 1);
+    arrButton.splice(0, 0, elementPrompt);
+    editor.Panels.removePanel('options');
+    editor.Panels.addPanel({
+      id: 'options',
+      visible: true,
+      buttons: arrButton,
+    });
+
+    let copySettings = editor.Panels.getButton('views', 'open-tm');
+    copySettings.attributes.label = 'Settings';
+    copySettings.attributes.className = 'button-view-style';
+    editor.Panels.removeButton('views', 'open-tm');
+    editor.Panels.addButton('views', copySettings);
+
+    let layers = editor.Panels.getButton('views', 'open-layers');
+    layers.attributes.label = 'Layers';
+    layers.attributes.className = 'button-view-style';
+    editor.Panels.removeButton('views', 'open-layers');
+    editor.Panels.addButton('views', layers);
+
+    let design = editor.Panels.getButton('views', 'open-sm');
+    design.attributes.className = 'button-view-style';
+    design.attributes.label = 'Design';
+
+    let blocks = editor.Panels.getButton('views', 'open-blocks');
+    blocks.attributes.className = 'button-view-style';
+    blocks.attributes.label = 'Blocks';
+
+  }, []);
+
+  return <div id='gjs'></div>;
+};
+
+export default Editor;
