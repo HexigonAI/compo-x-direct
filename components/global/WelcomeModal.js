16fb455104f740471a3222669312c288ebae84c3
nbrandon62 <nbrandon62@gmail.com>
2023-05-01 16:35:32 -0700

    changed the text of the input modal to explain to the user what to do

--- components/global/WelcomeModal.js
+++ components/global/WelcomeModal.js
@@ -17,22 +17,22 @@ export const WelcomeModal = ({
         <div className=" px-4 pb-4 pt-2 ">
           <div className="sm:flex sm:items-start">
             <div className=" ">
-              <h3 className="text-base font-semibold  text-white" id="modal-title">Deactivate account</h3>
+              <h3 className="text-base font-semibold  text-white" id="modal-title">Welcome to CompoX</h3>
               <div className=" w-96">
                 <p className="text-xs font-mono text-white">
-                As the next generation of ai-based visual editors for the web, we are proud to release our beta 1.2 version to yoursel and your team. Take a moment to watch how different our web creation experience is compared to anything else out there.  
-                <br/> <br/>Then, start by adding a prompt with proper, HTML based cues such as..
+                As the next generation of ai-based visual editors for the web, we are proud to release our beta 1.2 version to yourself and your team. Take a moment to watch how different our web creation experience is compared to anything else out there.  
+                <br/> <br/>Our engine will return responses depending on your input. Describe the component you want, and what kind of site you'll be using it for. Add a prompt using proper HTML cues like this...
                 </p> 
               </div>
               <div className="text-xs text-white flex  justify-center flex-col items-center">
-                <p>Start with an example prompt:</p>
-                <div className=' w-96 p-2  rounded-xl bg-[#F7F7F7] text-black'> Create a Hero Section </div>
+                <div className=' w-96 p-2  rounded-xl bg-[#F7F7F7] text-black'> Create a hero section for an e-commerce site with an intro on the left and a graphic on the right </div>
               </div>
             </div>
           </div>
         </div>
+        {/* TODO add in a more detailed prompt to get a better response */}
         <div className="bg-[#6366F1] px-4 py-2 sm:flex sm:flex-row-reverse sm:px-6">
-          <button onClick={(e) =>{ fetchPromptData(e, 'Create a Hero section'); setShowWelcome(false); console.log('test');}} type="button" className=" bg-transparent w-full text-white">Generate</button>
+          <button onClick={(e) =>{ fetchPromptData(e, 'Create a Hero section'); setShowWelcome(false)}} type="button" className=" bg-transparent w-full text-white">Generate</button>
         </div>
       </div>
     </div>
